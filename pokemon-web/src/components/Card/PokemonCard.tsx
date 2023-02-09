8348f2fedea33d88b8ac35207f0d0405112e3862
Jonas Nogueira de Oliveira <jonasnoliveirabr@gmail.com>
2023-02-09 16:01:59 -0300

    v2.1

--- pokemon-web/src/components/Card/PokemonCard.tsx
+++ pokemon-web/src/components/Card/PokemonCard.tsx
@@ -3,29 +3,36 @@ import {
   CardHeader,
   CardBody,
   Typography,
+  Button,
 } from "@material-tailwind/react";
 import { Pokemon } from "interface";
+import { FaHeart } from "react-icons/fa";
 
 export default function PokemonCard(props: Pokemon) {
-  const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${props.id}.png`
+  const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${props.id}.png`;
 
   return (
-    <Card className="w-56 border-2 border-blue-400">
+    <Card className="w-68 block border-2 border-blue-400">
       <CardHeader color="blue" className="relative h-40">
-        <Typography variant="h5" color="gray" className="m-2">
-          #{props.id}
-        </Typography>
+        <div className="flex flex-row items-center justify-between">
+          <Typography variant="h5" color="gray" className="m-2">
+            #{props.id}
+          </Typography>
+          <Button>
+            <FaHeart />
+          </Button>
+        </div>
         <img src={image} alt="img-blur-shadow" className="h-full w-full" />
       </CardHeader>
       <CardBody className="flex items-center justify-center p-3">
-        <Typography variant="h5" className="m-2">
+        <button className="w-24 m-1 mt-6 border-2 border-blue-700 rounded-md p-2">
           {props.name}
-        </Typography>
-        {props.types.map(pokemonType =>
-        <Typography variant="h5" color="gray" className="m-2">
-           {pokemonType.type.name}
-        </Typography>
-        )}
+        </button>
+        {props.types.map((pokemonType) => (
+          <button className="w-24 m-1 mt-6 border-2 border-blue-700 rounded-md p-2">
+            {pokemonType.type.name}
+          </button>
+        ))}
       </CardBody>
     </Card>
   );
--- pokemon-web/src/pages/Home.tsx
+++ pokemon-web/src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@material-tailwind/react";
-import axios from "axios";
 import PokemonCard from "components/Card/PokemonCard";
 import { Pagination } from "components/Pagination";
 import Sidebar from "components/Side/Side";
@@ -10,12 +9,12 @@ import Api from "service/Api";
 function HomePage() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
   const [offset, setOffset] = useState(0);
-  const limit = 18;
+  const [count, setCount] = useState(0);
+  const limit = 198;
 
   function next() {
     const newOffset = offset + limit;
     setOffset(newOffset);
-    console.log(newOffset);
   }
   function prev() {
     const newOffset = offset - limit;
@@ -45,7 +44,7 @@ function HomePage() {
     }
 
     getAllPokemons();
-  }, [offset]);
+  }, [offset, setCount]);
 
   const [query, setQuery] = useState("");
 
@@ -54,42 +53,23 @@ function HomePage() {
     setQuery(enteredName);
   };
 
-  async function getAllPokemons() {
-      const response = await Api.get(
-        `/pokemon/?limit=${limit}&offset=${offset}`
-      );
-      const { results } = response.data;
-
-      const payloadPokeemons = await Promise.all(
-        results.map(async (pokemon: Pokemon) => {
-          const { id, types } = await getMoreInfo(pokemon.url);
-
-          return {
-            name: pokemon.name,
-            id,
-            types,
-          };
-        })
-      );
-      setPokemons(payloadPokeemons);
-    }
-
-
   const search = () => {
-    const foundItems = pokemons.filter((item) =>
+    let foundName = pokemons.filter((item) =>
       item.name.toLowerCase().includes(query.toLowerCase())
     );
-    setPokemons(foundItems);
-    // const foundId = pokemons.filter((item) =>
-    //   item.id.toString().includes(query.toLowerCase())
-    // );
-    // setPokemons(foundId);
-    // const foundTypes = pokemons.filter((item) =>
-    //   item.types.map((pokemonType) => {
-    //     pokemonType.type.name.toString().includes(query.toLowerCase())
-    //   })
-    // );
-    // setPokemons(foundTypes);
+    console.log("foundName", foundName);
+    // let foundId = pokemons.filter((item) => {
+    //   item.id.toString().includes(query.toLowerCase());
+    // });
+    // console.log("foundId", foundId);
+    let foundTypes = pokemons.filter((item) =>
+      item.types.map((pokemonType) => {
+        pokemonType.type.name.includes(query.toLowerCase());
+      })
+    );
+    console.log("foundTypes", foundTypes);
+    // let foundItems = [...foundName, ...foundId, ...foundTypes];
+    setPokemons(foundName);
   };
 
   async function getMoreInfo(url: string): Promise<Request> {
@@ -110,7 +90,7 @@ function HomePage() {
           className="w-48 mx-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           value={query}
           onChange={inputHandler}
-          placeholder="Pesquise por nome ou tipo"
+          placeholder="Pesquise o nome"
         />
 
         <Button className="w-28 bg-blue-600" onClick={search}>
